e6c3b3c4bdc3160c1a4618176a9ef9c0d2def99c
dyechang256 <dangomesii65536@gmail.com>
2025-03-18 14:21:44 +0900

    時間割表を保存するための型を追記

--- src/types.mjs
+++ src/types.mjs
@@ -2,4 +2,7 @@
  * @typedef ClassData
  * @property {string} id//（プロパティ名）科目の管理
  * @property {string} name//科目名
+ * @typedef TableData
+ * @property {string} dayperiod `<曜日>-<時間>`の形式のテキスト
+ * @property {string} classId 科目のID
  */

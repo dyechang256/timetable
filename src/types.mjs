98cdce2f54b65033d499970c9304fe75deb20700
dyechang256 <dangomesii65536@gmail.com>
2025-03-18 15:29:43 +0900

    時間割票を保存するための型を作成

--- src/types.mjs
+++ src/types.mjs
@@ -2,6 +2,8 @@
  * @typedef ClassData
  * @property {string} id//（プロパティ名）科目の管理
  * @property {string} name//科目名
+ */
+/**
  * @typedef TableData
  * @property {string} dayperiod `<曜日>-<時間>`の形式のテキスト
  * @property {string} classId 科目のID

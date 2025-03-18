5b70ee2793513c650a6533d5aa2ae272f6eb05b8
dyechang256 <dangomesii65536@gmail.com>
2025-03-18 15:30:43 +0900

    時間割の詳細を登録

--- src/register.mjs
+++ src/register.mjs
@@ -3,9 +3,11 @@ import { ClassEditPage } from "./pages/class-edit.mjs";
 import { ClassListPage } from "./pages/class-list.mjs";
 import { ClassListItemComponent } from "./components/class-list-item.mjs";
 import { TimetableComponent } from "./components/timetable.mjs";
+import { TimetableDetailComponent } from "./components/timetable-detail.mjs";
 
 customElements.define("home-page", HomePage);
 customElements.define("class-list-page", ClassListPage);
 customElements.define("class-edit-page", ClassEditPage);
 customElements.define("class-list-item", ClassListItemComponent);
 customElements.define("timetable-component", TimetableComponent);
+customElements.define("timetable-detail", TimetableDetailComponent);
